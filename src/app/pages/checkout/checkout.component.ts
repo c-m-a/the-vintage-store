e899381008faf7a4a3625b141b9b0ff5590f78c6
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-03-12 22:26:31 -0500

    Add html components to Checkout page and Order model

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { Order } from '../../models/Order';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  ordersUrl:string = 'http://localhost:5000/orders';
 
-  constructor() { }
+  order: Order = {
+    product: {
+      price: 0,
+      tax: 0,
+      isOnSale: false
+    },
+    total: 0
+  };
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+    if(history.state.product === undefined)
+      this.router.navigate(['']);
+
+    this.order.product = history.state.product;
+    this.order.total = history.state.product.price;
+
+    if (history.state.product.id === 1) {
+      if (history.state.product.isOnSale) {
+        this.order.total = this.order.product.tax + 0.8 * this.order.total;
+      } else {
+        this.order.total += this.order.product.tax;
+      }
+    }
   }
 
 }

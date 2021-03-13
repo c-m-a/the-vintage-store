455c62eaf875efaea1570ea9c1116cc5217f32d2
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-03-12 20:23:52 -0500

    Add and connect Product model to Home page

---
+++ src/app/models/Product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  id?: number;
+  name?: string;
+  image?: string;
+  price: number = 0;
+  tax: number = 0;
+  isOnSale: Boolean = false;
+}
+
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../../models/Product';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  product: Product = {
+    price: 0,
+    tax: 0,
+    isOnSale: false
+  };
 
   constructor() { }
 
